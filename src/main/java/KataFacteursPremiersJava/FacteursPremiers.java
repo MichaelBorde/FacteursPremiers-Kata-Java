d0f89dac4212711f2fd8affb8b517c5b6922a01b
Michael Borde <michael.borde@gmail.com>
2013-04-15 20:27:17 +0200

    refacto

--- src/main/java/KataFacteursPremiersJava/FacteursPremiers.java
+++ src/main/java/KataFacteursPremiersJava/FacteursPremiers.java
@@ -7,7 +7,7 @@ import com.google.common.collect.Lists;
 public class FacteursPremiers {
 
 	public List<Integer> pour(int nombre) {
-		List facteurs = Lists.newArrayList();
+		List<Integer> facteurs = Lists.newArrayList();
 		for (int i = 2; i <= nombre; i++) {
 			for (; nombre % i == 0; nombre /= i) {
 				facteurs.add(i);
